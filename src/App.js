73055476994a573376209cff46349ceb117524d8
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2025-04-17 14:51:25 +0300

    firebase.config.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import db from "./firebase";
+import db from "./ firebase.config";
 import { collection, onSnapshot, addDoc } from "firebase/firestore";
 
 function App() {
